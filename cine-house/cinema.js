ec62733e6b07ceb917bce63f4d830d324560794e
hanakathy <khanamartins@gmail.com>
2022-05-25 20:45:50 -0400

    adicionou cine-house com nodemon

---
+++ cine-house/cinema.js
@@ -0,0 +1,8 @@
+//Aula de nodemon (25/05)
+
+console.log("Cinema da Sebastian");
+
+/* instalar nodemon: npm install nodemon --save
+    no terminal: npx nodemon [nome do arquivo].js
+    Aí o terminal atualizar assim que o arquivo alterado é salvo
+*/
